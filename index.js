f3e7f624ae0d933e0aa5358eaf4322ef8a93df4a
Earnest Angel <57413115+earnestangel@users.noreply.github.com>
2023-02-28 07:33:52 +0800

    Update config for Heroku deployment (#15)
    * Enable changing the server port with .env
    
    * Heroku deployment script
    
    * Update package.json
    
    * Add Heroku post-build script
    
    * Copy config file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: ".env" });
 const buildDir = "./dist";
 const NODE_ENV = process.env.NODE_ENV || "development";
+const port = process.env.PORT || 3000;
 const { start } = require(`./${buildDir}/ririko`);
 const colors = require("colors");
 
@@ -10,7 +11,7 @@ const colors = require("colors");
   // Host the bot:
   require("http")
     .createServer((req, res) => res.end("Ready."))
-    .listen(3000);
+    .listen(port);
 
   console.log("NODE_ENV:".red, NODE_ENV);
 })();
