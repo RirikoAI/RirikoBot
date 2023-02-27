f7073a11e93a30574ac7b2e84ede1dd8c00d40b7
Earnest Angel <57413115+earnestangel@users.noreply.github.com>
2023-02-27 23:39:24 +0800

    Add support for OpenAI (#11)

---
+++ src/app/Providers/AI/OpenAIProvider.js
@@ -0,0 +1,51 @@
+const { AIProviderBase } = require("app/Providers/AIProviderBase");
+const config = require("config");
+const getconfig = require("utils/getconfig");
+const openai = require("openai");
+const { OpenAIApi, Configuration } = require("openai");
+
+class OpenAIProvider extends AIProviderBase {
+  constructor() {
+    super();
+    this.token = getconfig.AIToken();
+
+    if (this.token === null) {
+      console.error("No token received");
+      return;
+    }
+
+    this.configuration = new Configuration({
+      apiKey: this.token,
+    });
+
+    this.openAiClient = new OpenAIApi(this.configuration);
+  }
+
+  getClient() {
+    return this.openAiClient;
+  }
+
+  async sendChat(messageText, context, history) {
+    const prompt = context + history;
+    try {
+      // Send request to NLP Cloud.
+      const response = await this.openAiClient.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.5,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+        stop: ["Human:"],
+      });
+
+      const answer = response?.data.choices[0].text.split("Robot:")[1]?.trim();
+      return answer;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
+
+module.exports = { OpenAIProvider };
