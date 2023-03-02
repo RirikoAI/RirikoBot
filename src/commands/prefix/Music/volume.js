a92ade5099ebb97b3edb7629e14440529c52844b
earnestangel <fareast.sn@gmail.com>
2023-03-02 19:22:22 +0800

    RA19 - Implement ability to set volume per guild. - Setting volume now saves to db - Can set the volume without being in a vc - Will auto read the volume settings from db on new play

--- src/commands/prefix/Music/volume.js
+++ src/commands/prefix/Music/volume.js
@@ -4,20 +4,31 @@ module.exports = {
     aliases: ["v", "set", "set-volume"],
   },
   inVoiceChannel: true,
-  run: async (client, message, args) => {
+  run: async (client, message, args, prefix, config, db) => {
     const queue = client.player.getQueue(message);
-    if (!queue)
-      return message.channel.send(
-        `${client.config.emoji.error} | There is nothing in the queue right now!`
-      );
+
+    if (!args[0]) {
+      const volume = +(await db.get(
+        `guild_volume_${message.channel.guild.id}`
+      ));
+      message.channel.send(`Current volume set was \`${volume}\``);
+      return;
+    }
+
     const volume = parseInt(args[0]);
     if (isNaN(volume))
       return message.channel.send(
         `${client.config.emoji.error} | Please enter a valid number!`
       );
-    queue.setVolume(volume);
+
+    const newVolume = await db.set(`guild_volume_${message.guild.id}`, args[0]);
+
+    try {
+      queue.setVolume(volume);
+    } catch (e) {}
+
     message.channel.send(
-      `${client.config.emoji.success} | Volume set to \`${volume}\``
+      `${client.config.emoji.success} | Volume set to \`${newVolume}\``
     );
   },
 };
