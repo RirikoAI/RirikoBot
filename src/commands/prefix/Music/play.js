590dcdcd19982ec2aa0b08e9cba6f7dddcc30ee2
Earnest Angel <57413115+earnestangel@users.noreply.github.com>
2023-03-02 18:52:43 +0800

    RA22 - Auto Issue /time command when a song is playing (with some additions) (#48)

--- src/commands/prefix/Music/play.js
+++ src/commands/prefix/Music/play.js
@@ -10,15 +10,25 @@ module.exports = {
   owner: false,
   inVoiceChannel: true,
   run: async (client, message, args) => {
-    const string = args.join(" ");
-    if (!string)
-      return message.channel.send(
-        `${client.config.emoji.error} | Please enter a song url or query to search.`
-      );
-    client.player.play(message.member.voice.channel, string, {
-      member: message.member,
-      textChannel: message.channel,
-      message,
-    });
+    try {
+      const string = args.join(" ");
+      if (!string)
+        return message.channel.send(
+          `${client.config.emoji.error} | Please enter a song url or query to search.`
+        );
+
+      if (message.member.voice.channel === null) {
+        await message.channel.send(
+          "You must be in a voice channel to play a music."
+        );
+        return;
+      }
+
+      client.player.play(message.member.voice.channel, string, {
+        member: message.member,
+        textChannel: message.channel,
+        message,
+      });
+    } catch (e) {}
   },
 };
