2e6d016ebdd5e5272bd10438046193c86bd8b3f2
Earnest Angel <57413115+earnestangel@users.noreply.github.com>
2023-03-03 04:15:27 +0800

    RA35 - Anime reactions (#54)

---
+++ src/commands/prefix/Anime/react.js
@@ -0,0 +1,108 @@
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const { getLang } = require("utils/language");
+const { getJson } = require("utils/httpUtils");
+const NekosLife = require("nekos.life");
+const neko = new NekosLife();
+
+const choices = [
+  "hug",
+  "kiss",
+  "cuddle",
+  "feed",
+  "pat",
+  "poke",
+  "slap",
+  "smug",
+  "tickle",
+  "wink",
+];
+
+const suffix = {
+  hug: "hugged",
+  kiss: "kissed",
+  cuddle: "cuddled",
+  feed: "fed",
+  pat: "patted",
+  poke: "poked",
+  slap: "slapped",
+  smug: "smugged at",
+  tickle: "ticked",
+  wink: "winked at",
+};
+
+module.exports = {
+  config: {
+    name: "react",
+    description:
+      "Send reaction anime images. Must be one of " + choices.join(", "),
+    usage: "react [choice]",
+  },
+  permissions: ["SendMessages"],
+  owner: false,
+  run: async (client, message, args, prefix) => {
+    if (!args[0]) {
+      return message.reply(
+        `Invalid command. Try \`${prefix}react ${choices.join(", ")}\``
+      );
+    }
+
+    const category = args[0].toLowerCase();
+    if (!choices.includes(category)) {
+      return message.reply(
+        `Invalid choice: \`${category}\`.\nAvailable reactions: ${choices.join(
+          ", "
+        )}`
+      );
+    }
+
+    let embed;
+    if (args[1]) {
+      embed = await genReaction(category, message.author, args[1]);
+    } else {
+      embed = await genReaction(category, message.author);
+    }
+
+    await message.reply({ embeds: [embed] });
+  },
+};
+
+/**
+ * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+ * @param category
+ * @param user
+ * @returns {Promise<EmbedBuilder>}
+ */
+const genReaction = async (category, user, secondUser = false) => {
+  try {
+    let imageUrl;
+
+    // some-random api
+    if (category === "wink") {
+      const response = await getJson("https://some-random-api.ml/animu/wink");
+      if (!response.success) throw new Error("API error");
+      imageUrl = response.data.link;
+    }
+
+    // neko api
+    else {
+      imageUrl = (await neko[category]()).url;
+    }
+
+    let response = new EmbedBuilder()
+      .setImage(imageUrl)
+      .setColor("Random")
+      .setFooter({ text: `Requested By ${user.tag}` });
+
+    if (secondUser) {
+      response.setDescription(`${user} ${suffix[category]} ${secondUser}`);
+    }
+
+    return response;
+  } catch (ex) {
+    return new EmbedBuilder()
+      .setColor("Red")
+      .setDescription("Failed to fetch meme. Try again!")
+      .setFooter({ text: `Requested By ${user.tag}` });
+  }
+};
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,114 @@
-const colors = require("colors");
 const config = require("config");
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+const pino = require("pino");
 
-const logger = (log, logLevel) => {};
+const webhookLogger = process.env.ERROR_LOGS
+  ? new WebhookClient({ url: process.env.ERROR_LOGS })
+  : undefined;
 
-module.exports = { logger };
+const today = new Date();
+const pinoLogger = pino.default(
+  {
+    level: "debug",
+  },
+  pino.multistream([
+    {
+      level: "info",
+      stream: pino.transport({
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "yyyy-mm-dd HH:mm:ss",
+          ignore: "pid,hostname",
+          singleLine: false,
+          hideObject: true,
+          customColors: "info:blue,warn:yellow,error:red",
+        },
+      }),
+    },
+    {
+      level: "debug",
+      stream: pino.destination({
+        dest: `${process.cwd()}/logs/combined-${today.getFullYear()}.${today.getMonth()}.${today.getDate()}.log`,
+        sync: true,
+      }),
+    },
+  ])
+);
+
+/**
+ * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+ * @param content
+ * @param err
+ */
+function sendWebhook(content, err) {
+  if (!content && !err) return;
+  const errString = err?.stack || err;
+
+  const embed = new EmbedBuilder()
+    .setColor("Red")
+    .setAuthor({ name: err?.name || "Error" });
+
+  if (errString)
+    embed.setDescription(
+      "```js\n" +
+        (errString.length > 4096
+          ? `${errString.substr(0, 4000)}...`
+          : errString) +
+        "\n```"
+    );
+
+  embed.addFields({
+    name: "Description",
+    value: content || err?.message || "NA",
+  });
+  webhookLogger.send({ username: "Logs", embeds: [embed] }).catch((ex) => {});
+}
+
+module.exports = class Logger {
+  /**
+   * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+   * @param {string} content
+   */
+  static success(content) {
+    pinoLogger.info(content);
+  }
+
+  /**
+   * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+   * @param {string} content
+   */
+  static log(content) {
+    pinoLogger.info(content);
+  }
+
+  /**
+   * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+   * @param {string} content
+   */
+  static warn(content) {
+    pinoLogger.warn(content);
+  }
+
+  /**
+   * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+   * @param {string} content
+   * @param {object} ex
+   */
+  static error(content, ex) {
+    if (ex) {
+      pinoLogger.error(ex, `${content}: ${ex?.message}`);
+    } else {
+      pinoLogger.error(content);
+    }
+    if (webhookLogger) sendWebhook(content, ex);
+  }
+
+  /**
+   * @author saiteja-madha https://github.com/saiteja-madha/discord-js-bot
+   * @param {string} content
+   */
+  static debug(content) {
+    pinoLogger.debug(content);
+  }
+};
