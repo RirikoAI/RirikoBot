472e32522d1faf2b457cd8f08169acc36baa6b35
earnestangel <fareast.sn@gmail.com>
2023-03-05 01:51:21 +0800

    RA65 - Anime Wallpaper finder quickfix

--- src/commands/prefix/Anime/wallpaper.js
+++ src/commands/prefix/Anime/wallpaper.js
@@ -21,52 +21,59 @@ module.exports = {
   },
   owner: false,
   run: async (client, message, args) => {
-    const lang = getLang();
-    const query = args.join(" ");
-    if (!query)
-      return message.reply("Please give me an anime to search a wallpaper!");
-    async function Wallpaper1() {
-      let wallpaper = await wall.getAnimeWall5(query);
-      return wallpaper;
-    }
-
-    let wallpapers;
-
     try {
-      wallpapers = await Wallpaper1();
-    } catch (err) {
-      console.log(err);
+      const lang = getLang();
+      const query = args.join(" ");
+      if (!query)
+        return message.reply("Please give me an anime to search a wallpaper!");
+
+      let wallpapers;
+
+      try {
+        wallpapers = await Wallpaper1(query);
+      } catch (err) {
+        return message.reply(
+          "❌ Sorry, I couldn't find any wallpaper with the keyword: " +
+            query.toString()
+        );
+      }
+
+      let bigImages = [];
+
+      wallpapers.forEach((wallpaper, index, arr) => {
+        if (wallpaper?.image?.startsWith("http")) {
+          bigImages.push(wallpaper);
+        }
+      });
+
+      const randomImage =
+        bigImages[Math.floor(Math.random() * bigImages.length)];
+
+      const embed = new EmbedBuilder()
+        .setColor("Random")
+        .setDescription(`Requested by: ${message.author.toString()}`)
+        .setImage(randomImage.image)
+        .addFields({
+          name: "Full: ",
+          value: randomImage.image,
+        })
+        .setTimestamp()
+        .setURL(randomImage.image)
+        .setFooter({ text: `${lang.footer1}` });
+
+      message.reply({
+        embeds: [embed],
+      });
+    } catch (e) {
       return message.reply(
         "❌ Sorry, I couldn't find any wallpaper with the keyword: " +
           query.toString()
       );
     }
-
-    let bigImages = [];
-
-    wallpapers.forEach((wallpaper, index, arr) => {
-      if (wallpaper?.image?.startsWith("http")) {
-        bigImages.push(wallpaper);
-      }
-      return;
-    });
-
-    const randomImage = bigImages[Math.floor(Math.random() * bigImages.length)];
-
-    const embed = new EmbedBuilder()
-      .setColor("Random")
-      .setDescription(`Requested by: ${message.author.toString()}`)
-      .setImage(randomImage.image)
-      .addFields({
-        name: "Full: ",
-        value: randomImage.image,
-      })
-      .setTimestamp()
-      .setURL(randomImage.image)
-      .setFooter({ text: `${lang.footer1}` });
-
-    message.reply({
-      embeds: [embed],
-    });
   },
 };
+
+async function Wallpaper1(query) {
+  let wallpaper = await wall.getAnimeWall5(query);
+  return wallpaper;
+}
