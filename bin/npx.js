28dfc2f3899f6c794a6a92f8632e33e62e30f116
Earnest Angel <57413115+earnestangel@users.noreply.github.com>
2023-06-19 13:44:38 +0800

    Fixes for Windows installations. Release candidate  (#143)
    * Add NPX cli command. Release candidate
    
    * Fixes for Windows installations. Release candidate

--- bin/npx.js
+++ bin/npx.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
+console.log("================= Welcome to Ririko AI! =================");
+
 const { execSync } = require("child_process");
+
+let copyCommand;
+if (process.platform === "win32") {
+  // User is running the script on Windows
+  console.log("Running on Windows");
+  copyCommand = "copy";
+} else {
+  // User is running the script on Linux/Unix or macOS
+  console.log("Running on Linux/Unix or macOS");
+  copyCommand = "cp";
+}
 
 const runCommand = (command) => {
   try {
@@ -12,20 +25,26 @@ const runCommand = (command) => {
   return true;
 };
 
-const dirName = process.argv[2];
+const dirName = process.argv[2] ? process.argv[2] : "RirikoAI";
 const gitCheckoutCommand = `git clone https://github.com/RirikoAI/RirikoBot ${dirName}`;
 const installDepsCommands = `cd ${dirName} && npm i --include=dev`;
-const buildCommands = `cp config.example.js config.js && cp .env.example .env && npm run build`;
+const buildCommands = `${copyCommand} config.example.js config.js && ${copyCommand} .env.example .env && npm run build`;
 
-console.log(`Cloning into ${dirName}...`);
+console.log(
+  `\n\n================= Cloning into ${dirName}... =================`
+);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing dependencies for ${dirName}`);
+console.log(
+  `\n\n================= Installing dependencies for ${dirName}... =================`
+);
 const installedDeps = runCommand(installDepsCommands);
 if (!installedDeps) process.exit(-1);
 
-console.log(`Running build commands for ${dirName}`);
+console.log(
+  `\n\n================= Running build commands for ${dirName}... =================`
+);
 const buildSuccess = runCommand(buildCommands);
 if (!buildSuccess) process.exit(-1);
 
