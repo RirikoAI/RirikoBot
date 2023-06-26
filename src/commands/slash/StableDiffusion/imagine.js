f0ab0e867a4070cc84883a3f64d9d54f837e5e48
Earnest Angel <fareast.sn@gmail.com>
2023-06-26 18:05:26 +0800

    Refactor imagine command

--- src/commands/slash/StableDiffusion/imagine.js
+++ src/commands/slash/StableDiffusion/imagine.js
@@ -13,10 +13,8 @@ const {
 import Replicate from "replicate";
 import language from "languages/en";
 import { StableDiffusion } from "config";
-import axios from "axios";
+import axios, { get } from "axios";
 import { AttachmentBuilder } from "discord.js";
-
-let replicate = null;
 
 module.exports = {
   name: "imagine",
@@ -32,84 +30,112 @@ module.exports = {
     },
   ],
   type: 1,
-  run: async (client, interaction, args, prefix, config, db) => {
-    await interaction.deferReply();
-    // create replicate client
-    const replicate = new Replicate({
-      // get your token from https://replicate.com/account
-      auth: replicateToken(),
-    });
-
-    const userPrompt = interaction.options.getString("prompt");
-
-    try {
-      const model = StableDiffusion.AvailableModels[StableDiffusion.Model];
-      const input = {
-        prompt: userPrompt,
-        width: StableDiffusion.Width,
-        height: StableDiffusion.Height,
-        negative_prompt: StableDiffusion.NegativePrompt,
-        disable_safety_check: true,
-        num_inference_steps: StableDiffusion.NumInferenceSteps,
-        guidance_scale: StableDiffusion.GuidanceScale,
-        scheduler: StableDiffusion.Scheduler,
-      };
-
-      const output = await replicate.run(model, { input });
-
-      const imageUrl = output.toString();
-
-      const response = await axios.get(imageUrl, {
-        responseType: "arraybuffer",
-      });
-      const imageBuffer = response.data;
-
-      const attachment = new AttachmentBuilder(imageBuffer, {
-        name: "image.png",
-      });
-
-      const embed = new EmbedBuilder()
-        .setImage("attachment://image.png")
-        .setTitle("Here's your image")
-        .setDescription(`**Prompts:**\n${userPrompt}`)
-        .setColor("#00C853")
-        .setFooter({
-          text: `Requested by: ${interaction.user.username} | ${language.footer1}`,
-          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-        });
-
-      const embedRow = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setURL(imageUrl)
-          .setLabel(`Download Image`)
-          .setStyle(ButtonStyle.Link),
-        new ButtonBuilder()
-          .setURL("https://angel.net.my/prompts.php")
-          .setLabel("Example Prompts")
-          .setStyle(ButtonStyle.Link)
-      );
-
-      await interaction.editReply({
-        embeds: [embed],
-        components: [embedRow],
-        files: [attachment],
-      });
-    } catch (e) {
-      console.error(
-        "An error occured when trying to generate image with Stable Diffusion.",
-        e
-      );
-      if (e.message.includes("NSFW")) {
-        await interaction.editReply(
-          "NSFW content detected. Please retry or change your prompt."
-        );
-      } else if (e.message.includes("ExperimentalWarning")) {
-        // k
-      } else {
-        await interaction.editReply(
-          "Something went wrong. Please check your console for errors."
-        );
-      }
-    }
-  },
+  run: imagineCommand,
 };
+
+async function imagineCommand(client, interaction, args, prefix, config, db) {
+  await interaction.deferReply();
+  const replicate = createReplicateClient();
+
+  const userPrompt = interaction.options.getString("prompt");
+
+  try {
+    const output = await generateImage(replicate, userPrompt);
+
+    const imageUrl = output.toString();
+
+    const imageBuffer = await getImageBuffer(imageUrl);
+
+    const attachment = createAttachment(imageBuffer);
+
+    const embed = createEmbed(userPrompt, imageUrl, interaction.user);
+
+    const embedRow = createActionRow(imageUrl);
+
+    await interaction.editReply({
+      embeds: [embed],
+      components: [embedRow],
+      files: [attachment],
+    });
+  } catch (e) {
+    await handleErrorResponse(e, interaction);
+  }
+}
+
+function createReplicateClient() {
+  return new Replicate({
+    auth: replicateToken(),
+  });
+}
+
+async function generateImage(replicate, userPrompt) {
+  const model = StableDiffusion.AvailableModels[StableDiffusion.Model];
+  const input = {
+    prompt: userPrompt,
+    width: StableDiffusion.Width,
+    height: StableDiffusion.Height,
+    negative_prompt: StableDiffusion.NegativePrompt,
+    disable_safety_check: true,
+    num_inference_steps: StableDiffusion.NumInferenceSteps,
+    guidance_scale: StableDiffusion.GuidanceScale,
+    scheduler: StableDiffusion.Scheduler,
+  };
+
+  return await replicate.run(model, { input });
+}
+
+async function getImageBuffer(imageUrl) {
+  const response = await get(imageUrl, {
+    responseType: "arraybuffer",
+  });
+  return response.data;
+}
+
+function createAttachment(imageBuffer) {
+  return new AttachmentBuilder(imageBuffer, {
+    name: "image.png",
+  });
+}
+
+function createEmbed(userPrompt, imageUrl, user) {
+  return new EmbedBuilder()
+    .setImage("attachment://image.png")
+    .setTitle("Here's your image")
+    .setDescription(`**Prompts:**\n${userPrompt}`)
+    .setColor("#00C853")
+    .setFooter({
+      text: `Requested by: ${user.username} | ${language.footer1}`,
+      iconURL: user.displayAvatarURL({ dynamic: true }),
+    });
+}
+
+function createActionRow(imageUrl) {
+  return new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setURL(imageUrl)
+      .setLabel(`Download Image`)
+      .setStyle(ButtonStyle.Link),
+    new ButtonBuilder()
+      .setURL("https://angel.net.my/prompts.php")
+      .setLabel("Example Prompts")
+      .setStyle(ButtonStyle.Link)
+  );
+}
+
+async function handleErrorResponse(error, interaction) {
+  console.error(
+    "An error occurred when trying to generate image with Stable Diffusion.",
+    error
+  );
+  if (error.message.includes("NSFW")) {
+    await interaction.editReply(
+      "NSFW content detected. Please retry or change your prompt."
+    );
+  } else if (error.message.includes("ExperimentalWarning")) {
+    // Ignore experimental warnings
+  } else {
+    await interaction.editReply(
+      "Something went wrong. Please check your console for errors."
+    );
+  }
+}
