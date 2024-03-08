eb58f9ad69e2f883292219f25cf46cfc35a96300
Earnest Angel <fareast.sn@gmail.com>
2024-03-08 14:23:56 +0800

    [FEAT] Integrate React Dashboard + Backend API connector

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
 
   rules: {
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-ts-comment": "off",
   },
--- src/dashboard/utils/common.ts
+++ src/dashboard/utils/common.ts
@@ -1,4 +1,7 @@
-export function findLast<T>(array: T[], filter: (item: T) => boolean): T | null {
+export function findLast<T>(
+  array: T[],
+  filter: (item: T) => boolean
+): T | null {
   for (let index = array.length - 1; index >= 0; index--) {
     if (filter(array[index])) {
       return array[index];
@@ -10,8 +13,8 @@ export function findLast<T>(array: T[], filter: (item: T) => boolean): T | null 
 
 export function toRGB(num: number) {
   num >>>= 0;
-  let b = num & 0xff,
+  const b = num & 0xff,
     g = (num & 0xff00) >>> 8,
     r = (num & 0xff0000) >>> 16;
-  return 'rgb(' + [r, g, b].join(',') + ')';
+  return "rgb(" + [r, g, b].join(",") + ")";
 }
