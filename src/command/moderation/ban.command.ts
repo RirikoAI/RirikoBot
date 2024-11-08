305c53702816fefa0399e7bd2dbfbf0ab065a15b
EarnestAngel <fareast.sn@gmail.com>
2024-11-09 02:59:41 +0800

    [FEAT] Add basic moderation commands

---
+++ src/command/moderation/ban.command.ts
@@ -0,0 +1,71 @@
+import { CommandInterface } from '#command/command.interface';
+import { Command } from '#command/command.class';
+import { SlashCommandOptionTypes } from '#command/command.types';
+
+/**
+ * BanCommand
+ * @description Ban a user from the server.
+ * @category Command
+ * @author Earnest Angel (https://angel.net.my)
+ */
+export default class BanCommand extends Command implements CommandInterface {
+  name = 'ban';
+  regex = new RegExp('^ban$|^ban |^b ', 'i');
+  description = 'Ban a user from the server.';
+  category = 'moderation';
+  usageExamples = ['ban @user', 'b @user'];
+  slashOptions = [
+    {
+      name: 'user',
+      description: 'The user to ban.',
+      type: SlashCommandOptionTypes.User,
+      required: true,
+    },
+  ];
+
+  async runPrefix(message) {
+    // check if the sender has permission to ban members
+    if (
+      !message.member.permissions.has('BAN_MEMBERS') ||
+      !message.member.permissions.has('ADMINISTRATOR')
+    ) {
+      return message.reply('You do not have permission to ban members.');
+    }
+    // get the user to ban
+    const user = message.mentions.members.first();
+    if (!user) {
+      return message.reply('Please mention a user to ban.');
+    }
+    // check if the user can be banned
+    if (!user.bannable) {
+      return message.reply('This user cannot be banned.');
+    }
+    // ban the user
+    await user.ban();
+    // tell the user that the user has been banned
+    await message.reply(`Banned ${user.user.tag} from the server.`);
+  }
+
+  async runSlash(interaction) {
+    // check if the sender has permission to ban members
+    if (
+      !interaction.member.permissions.has('BAN_MEMBERS') ||
+      !interaction.member.permissions.has('ADMINISTRATOR')
+    ) {
+      return interaction.reply('You do not have permission to ban members.');
+    }
+    // get the user to ban
+    const user = interaction.options.getUser('user');
+    if (!user) {
+      return interaction.reply('Please mention a user to ban.');
+    }
+    // check if the user can be banned
+    if (!user.bannable) {
+      return interaction.reply('This user cannot be banned.');
+    }
+    // ban the user
+    await user.ban();
+    // tell the user that the user has been banned
+    await interaction.reply(`Banned ${user.tag} from the server.`);
+  }
+}
---
+++ src/command/moderation/kick.command.ts
@@ -0,0 +1,73 @@
+import { CommandInterface } from '#command/command.interface';
+import { Command } from '#command/command.class';
+import { SlashCommandOptionTypes } from '#command/command.types';
+
+/**
+ * KickCommand
+ * @description Kick a user from the server.
+ * @category Command
+ * @author Earnest Angel (https://angel.net.my)
+ */
+export default class KickCommand extends Command implements CommandInterface {
+  name = 'kick';
+  regex = new RegExp('^kick$|^kick |^k ', 'i');
+  description = 'Kick a user from the server.';
+  category = 'moderation';
+  usageExamples = ['kick @user', 'k @user'];
+  slashOptions = [
+    {
+      name: 'user',
+      description: 'The user to kick.',
+      type: SlashCommandOptionTypes.User,
+      required: true,
+    },
+  ];
+
+  async runPrefix(message) {
+    // check if the sender has permission to kick members
+    if (
+      !message.member.permissions.has('KICK_MEMBERS') ||
+      !message.member.permissions.has('ADMINISTRATOR')
+    ) {
+      return await message.reply('You do not have permission to kick members.');
+    }
+    // get the user to kick
+    const user = message.mentions.members.first();
+    if (!user) {
+      return await message.reply('Please mention a user to kick.');
+    }
+    // check if the user can be kicked
+    if (!user.kickable) {
+      return await message.reply('This user cannot be kicked.');
+    }
+    // kick the user
+    await user.kick();
+    // tell the user that the user has been kicked
+    await message.reply(`Kicked ${user.user.tag} from the server.`);
+  }
+
+  async runSlash(interaction) {
+    // check if the sender has permission to kick members
+    if (
+      !interaction.member.permissions.has('KICK_MEMBERS') ||
+      !interaction.member.permissions.has('ADMINISTRATOR')
+    ) {
+      return await interaction.reply(
+        'You do not have permission to kick members.',
+      );
+    }
+    // get the user to kick
+    const user = interaction.options.getMember('user');
+    if (!user) {
+      return await interaction.reply('Please mention a user to kick.');
+    }
+    // check if the user can be kicked
+    if (!user.kickable) {
+      return await interaction.reply('This user cannot be kicked.');
+    }
+    // kick the user
+    await user.kick();
+    // tell the user that the user has been kicked
+    await interaction.reply(`Kicked ${user.user.tag} from the server.`);
+  }
+}
