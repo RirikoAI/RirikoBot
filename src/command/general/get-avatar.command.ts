457d9c9f5f36836b5b2fc51cbc52e597378395c1
EarnestAngel <fareast.sn@gmail.com>
2024-11-10 22:17:26 +0800

    [FEAT] GetAvatar - Add option to select user in the interaction

--- src/command/general/get-avatar.command.ts
+++ src/command/general/get-avatar.command.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { EmbedBuilder } from 'discord.js';
 import { Command } from '#command/command.class';
 import { CommandInterface } from '#command/command.interface';
-import { DiscordInteraction, DiscordMessage } from '#command/command.types';
+import { DiscordInteraction, DiscordMessage, SlashCommandOptionTypes } from '#command/command.types';
 
 /**
  * GetAvatarCommand
@@ -21,16 +21,27 @@ export default class GetAvatarCommand
   category = 'general';
   usageExamples = ['get-avatar'];
 
+  slashOptions = [
+    {
+      name: 'user',
+      description: 'The user to get the avatar from',
+      type: SlashCommandOptionTypes.User,
+      required: false,
+    },
+  ];
+
   async runPrefix(message: DiscordMessage): Promise<void> {
     // Get the user's avatar based on the mentioned user
     const user = message.mentions.users.first() || message.author;
     const avatar = user.displayAvatarURL();
-    const embed = new EmbedBuilder().addFields([
-      {
-        name: `Avatar of ${user.username}`,
-        value: `[Click here to view](${avatar})`,
-      },
-    ]);
+    const embed = new EmbedBuilder()
+      .addFields([
+        {
+          name: `Avatar of ${user.username}`,
+          value: `[Click here to view](${avatar})`,
+        },
+      ])
+      .setImage(avatar);
 
     await message.reply({
       embeds: [embed],
@@ -41,12 +52,14 @@ export default class GetAvatarCommand
     // Get the user's avatar based on the mentioned user
     const user = interaction.options.getUser('user') || interaction.user;
     const avatar = user.displayAvatarURL();
-    const embed = new EmbedBuilder().addFields([
-      {
-        name: `Avatar of ${user.username}`,
-        value: `[Click here to view](${avatar})`,
-      },
-    ]);
+    const embed = new EmbedBuilder()
+      .addFields([
+        {
+          name: `Avatar of ${user.username}`,
+          value: `[Click here to view](${avatar})`,
+        },
+      ])
+      .setImage(avatar);
 
     await interaction.reply({
       embeds: [embed],
