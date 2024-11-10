88e8eab05413f9f1136ae82cbd44aee52db13e77
EarnestAngel <fareast.sn@gmail.com>
2024-11-10 21:46:29 +0800

    [FEAT] Create GetAvatar command

---
+++ src/command/general/get-avatar.command.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { EmbedBuilder } from 'discord.js';
+import { Command } from '#command/command.class';
+import { CommandInterface } from '#command/command.interface';
+import { DiscordInteraction, DiscordMessage } from '#command/command.types';
+
+/**
+ * GetAvatarCommand
+ * @author Earnest Angel (angel.net.my)
+ * @description Get the avatar of a user
+ * @category Command
+ */
+@Injectable()
+export default class GetAvatarCommand
+  extends Command
+  implements CommandInterface
+{
+  name = 'get-avatar';
+  regex = new RegExp('^get-avatar |^get-avatar$', 'i');
+  description = 'Get the avatar of a user';
+  category = 'general';
+  usageExamples = ['get-avatar'];
+
+  async runPrefix(message: DiscordMessage): Promise<void> {
+    // Get the user's avatar based on the mentioned user
+    const user = message.mentions.users.first() || message.author;
+    const avatar = user.displayAvatarURL();
+    const embed = new EmbedBuilder().addFields([
+      {
+        name: `Avatar of ${user.username}`,
+        value: `[Click here to view](${avatar})`,
+      },
+    ]);
+
+    await message.reply({
+      embeds: [embed],
+    });
+  }
+
+  async runSlash(interaction: DiscordInteraction): Promise<void> {
+    // Get the user's avatar based on the mentioned user
+    const user = interaction.options.getUser('user') || interaction.user;
+    const avatar = user.displayAvatarURL();
+    const embed = new EmbedBuilder().addFields([
+      {
+        name: `Avatar of ${user.username}`,
+        value: `[Click here to view](${avatar})`,
+      },
+    ]);
+
+    await interaction.reply({
+      embeds: [embed],
+    });
+  }
+}
