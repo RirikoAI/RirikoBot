ba6a504f85c0827e0dec19bcc759a00ffc9cd31e
Earnest Angel <fareast.sn@gmail.com>
2025-09-10 00:47:07 +0800

    Set default DISCORD_REDIRECT_URI

--- src/config/default/discord.config.ts
+++ src/config/default/discord.config.ts
@@ -2,6 +2,6 @@ export default {
   DISCORD_BOT_TOKEN: '',
   DISCORD_APPLICATION_ID: '',
   DEFAULT_PREFIX: '!',
-  DISCORD_REDIRECT_URI: '',
+  DISCORD_REDIRECT_URI: '/v1/auth/discord/redirect',
   DISCORD_OAUTH2_CLIENT_SECRET: '',
 };
